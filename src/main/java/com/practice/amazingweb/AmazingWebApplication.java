f7668aad7e488dbfd30fd80c7d1b5eae643ad17c
jayvimal <jayvimalathithan@gmail.com>
2017-11-30 09:53:58 +0530

    All in

---
+++ src/main/java/com/practice/amazingweb/AmazingWebApplication.java
@@ -0,0 +1,13 @@
+package com.practice.amazingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingWebApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello , World !");
+		SpringApplication.run(AmazingWebApplication.class, args);
+	}
+}
