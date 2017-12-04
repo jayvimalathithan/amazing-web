91de22dbd15ea52e2b004c270ef83513815e73b6
jayvimal <jayvimalathithan@gmail.com>
2017-12-04 19:06:26 +0530

    New chk in

--- src/main/java/com/practice/amazingweb/AmazingWebApplication.java
+++ src/main/java/com/practice/amazingweb/AmazingWebApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AmazingWebApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Hello , World Amaithi Amaithi!");
-		SpringApplication.run(AmazingWebApplication.class, args);
+	public static void main(String[] args) 
+        {
+	  System.out.println("Hello ,World Changed from diff src");
+	  SpringApplication.run(AmazingWebApplication.class, args);
 	}
 }
